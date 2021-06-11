20180b86a5691ce10b9409a743eb9e7b38fbd9e7
Samaila Bala <samailabalap@gmail.com>
2021-06-11 10:42:20 +0100

    add custom error

--- src/App.js
+++ src/App.js
@@ -2,20 +2,26 @@ import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Tracker from '@asayerio/tracker';
+import * as Sentry from '@sentry/react';
 
 const tracker = new Tracker({
   projectID: 8794100460112674,
+  onStart: ({ sessionToken }) => {
+    Sentry.setTag('openReplaySessionToken', sessionToken);
+  },
 });
 
 function App() {
   const [count, setCount] = useState(0);
   useEffect(() => {
-    if (window.asayer && window.asayer.id()) {
-      Raven.setTagsContext({ asayer_session_id: window.asayer.id() });
-    }
     tracker.start();
   }, []);
-  const increment = () => setCount((prevState) => (prevState += 1));
+  const increment = () => {
+    if (count % 5 === 0 && count !== 0) {
+      throw 'Custom error triggered, count a multiple of 5';
+    }
+    setCount((prevState) => (prevState += 1));
+  };
   const decrement = () => setCount((prevState) => (prevState -= 1));
   console.log(count);
   return (
